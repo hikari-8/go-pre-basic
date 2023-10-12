bf99d2cb5e49ecd1c4f26772aa6ede6c5ae514f6
hikari-8 <kanbarusuruga0127@gmail.com>
2023-10-12 14:36:15 +0900

    feature/operator

---
+++ operator/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+	// 算術演算子tips
+	i1 := 0
+	i1 += 4
+	fmt.Println(i1)
+
+	s1 := "ABC"
+	s1 += "DEF"
+	fmt.Println(s1)
+
+	i1++
+	fmt.Println(i1)
+	i1--
+	fmt.Println(i1)
+
+}
